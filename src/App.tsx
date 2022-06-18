a59596483e6e663c8ca0226087b2daef7034610d
brxndonedie <brandonedie0000@gmail.com>
2022-06-17 22:13:23 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'solid-app-router'
+import { Component, createEffect, createSignal, lazy } from 'solid-js'
+import Nav from './components/Nav'
+const Home = lazy(() => import('./pages/Home'))
+const SavedRepos = lazy(() => import('./pages/SavedRepos'))
+
+const [username, setUserName] = createSignal('brandonedie')
+const [repos, setRepos] = createSignal([])
+
+createEffect(async () => {
+  const res = await fetch(`https://api.github.com/users/${username()}/repos?sort=created`)
+  setRepos(await res.json())
+})
+
+const App: Component = () => {
+  return (
+    <div class="container">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favrepos" element={<SavedRepos />} />
+      </Routes>
+    </div>
+  )
+}
+
+export { username, setUserName, repos }
+export default App
